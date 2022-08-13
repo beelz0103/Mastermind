bf314e372ae9602ee8936d28c688e23b7b691b42
Praveen <anabathula.praveen103@gmail.com>
2022-08-13 15:25:48 +0530

    Update hint method fixing same color input bug

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,17 @@
+require 'pry-byebug'
+
 class Board
   attr_accessor :code, :solution, :board
 
   def initialize
-    @code = ["1", "2", "3", "4"]
+    @code = ["1", "2", "3", "4", "5", "6"]
     @solution = []
     @board = []
   end
 
   def codemaker(code)
     @solution = code.sample(4)
+    @solution = ["1", "1", "2", "2"]
   end
 
   def win?(board,turn)
@@ -49,8 +52,8 @@ class Game
   def start_game
     loop do
       arr = player_input
-      hint_arr = []
-      hint(arr,hint_arr)
+      sol_arr = arr.dup
+      hint_arr = hint(sol_arr)
       board.update_board(arr,hint_arr,board.board,turn)
       board.print_board(board.board)
       break if board.win?(board.board, turn)
@@ -59,17 +62,39 @@ class Game
     end
   end
 
-  def hint(arr, arr2)
-    board.solution.each_with_index do |element, index|
-      arr.each_with_index do |e,i|
-        if element == e && index == i
-          arr2 << "black"
-        elsif element == e
-          arr2 << "white"
-        end
-      end
+def perfect_match(solution, answer, hint_arr)
+  solution.each_with_index do |sol_color, sol_pos|
+    answer.each_with_index do |answer_color, answer_pos|
+      next unless sol_color == answer_color && sol_pos == answer_pos
+
+      solution[sol_pos] = 'o'
+      answer[answer_pos] = 'x'
+      hint_arr << 'red'
+      break
     end
   end
+end
+
+def wrong_positon(solution, answer, hint_arr)
+  solution.each_with_index do |sol_color, sol_pos|
+    answer.each_with_index do |answer_color, answer_pos|
+      next unless sol_color == answer_color
+
+      hint_arr << 'white'
+      solution[sol_pos] = 'o'
+      answer[answer_pos] = 'x'
+      break
+    end
+  end
+end
+
+def hint(answer)
+  hint = []
+  solution_2 = board.solution.dup
+  perfect_match(solution_2, answer, hint)
+  wrong_positon(solution_2, answer, hint)
+  hint
+end
 
   def player_input
     input = gets.chomp.split("")
